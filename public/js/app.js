590fda03e84962d1eaaffe98e4cce537886403be
allendavecatucag <catucagallenit1b@gmail.com>
2017-03-20 10:28:04 +0800

    my third commit

--- public/js/app.js
+++ public/js/app.js
@@ -18,16 +18,32 @@ $.ajax({
 }).done(function(res){
 	console.log(res);
 	let html = `
-	<center>
-		<h1>${res.Title}</h1>
-		<img src="${res.Poster}"/>
-		<h3>${res.Year}</h1>
-		<h5><p>${res.Plot}</p></h5>
-		<h5>Genre: ${res.Genre}</h5>
-		<h5>Writer: ${res.Writer}</h5>
-		<h5>Actors: ${res.Actors}</h5>
-	</center>
-	`;
+	 <center>
+    <h1>    
+Naruto Road to Ninja: Naruto the Movie
+</h1>
+      
+        <img src =${res.Poster}"/>
+ <h3> Rated: ${res.Rated}</h3>
+<h3> Released: ${res.Released}</h3>
+<h3> Runtime: ${res.Runtime}</h3>
+<h3> Genre: ${res.Genre}</h3>
+<h3> Director: ${res.Director}</h3>
+<h3> Writer: ${res.Writer}</h3>
+<h3> Language: ${res.Language}</h3>
+<h3> Country: ${res.Country}</h3>
+<h3> Awards: ${res.Awards}</h3>
+<h3> Metascore: ${res.Metascore}</h3>
+<h3> imdbRating: ${res.imdbRating}</h3>
+<h3> imdbVotes: ${res.imdbVotes}</h3>
+<p>Actors</p>
+<h4>${res.Actors}</h4>
+
+        <p>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/0zaurrISlgE" frameborder="0" allowfullscreen></iframe>
+        </p>
+   </center>
+   `;
 	$('#app').html(html);
 
 
@@ -41,31 +57,7 @@ $('#apps').html(`
 `);
 
 
-setTimeout(function(){
-	y();
-},3000);
 
-function y(){
-$.ajax({
-	url: "http://www.omdbapi.com/?i=tt1219289&plot=full"
-}).done(function(res){
-	console.log(res);
-	let html = `
-		<h1>${res.Title}</h1>
-		<img src="${res.Poster}"/>
-		<h3>${res.Year}</h1>
-		<h5><p>${res.Plot}</p></h5>
-		<h5>Genre: ${res.Genre}</h5>
-		<h5>Director: ${res.Director}</h5>
-		<h5>Writer: ${res.Writer}</h5>
-		<h5>Actors: ${res.Actors}</h5>
-
-	`;
-	$('#apps').html(html);
-
-
-});
-}
 
 
 
