36345493357bd56905ed2e06825591452ba9a95e
allendavecatucag <catucagallenit1b@gmail.com>
2017-03-20 10:01:06 +0800

    my third commit

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,12 @@
 $('#app').html(`
-	<div class="progress">
-      <div class="indeterminate"></div>
-  </div>
+    <div><h1>Loading...</h1></div>
+      <div class="cssload-tetrominos">
+    <div class="cssload-tetromino cssload-box1"></div>
+    <div class="cssload-tetromino cssload-box2"></div>
+    <div class="cssload-tetromino cssload-box3"></div>
+    <div class="cssload-tetromino cssload-box4"></div>
+</div>
 `);
-
 
 setTimeout(function(){
 	x();
@@ -11,10 +14,11 @@ setTimeout(function(){
 
 function x(){
 $.ajax({
-	url: "http://www.omdbapi.com/?i=tt4422836"
+	url: "http://www.omdbapi.com/?i=tt1703048"
 }).done(function(res){
 	console.log(res);
 	let html = `
+	<center>
 		<h1>${res.Title}</h1>
 		<img src="${res.Poster}"/>
 		<h3>${res.Year}</h1>
@@ -22,7 +26,7 @@ $.ajax({
 		<h5>Genre: ${res.Genre}</h5>
 		<h5>Writer: ${res.Writer}</h5>
 		<h5>Actors: ${res.Actors}</h5>
-
+	</center>
 	`;
 	$('#app').html(html);
 
